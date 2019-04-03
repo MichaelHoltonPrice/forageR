7edb746f0c504a4564cbcf7f1c7bd07e68c9f8de
Michael Holton Price <MichaelHoltonPrice@gmail.com>
2019-04-03 05:22:00 -0600

    Prototyped diet breadth code

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Resource data from Winterhalder et al. (1988): The Population Ecology of Hunter-Gatherers and Their Prey [mostly from Table 6].
+#'
+#' @format A data frame with 8 rows and 7 variables:
+#' \describe{
+#'   \item{name}{Resource name}
+#'   \item{energy}{Food energy of resource (kcal)}
+#'   \item{handling_time}{Time needed to pursue and process resource (min)}
+#'   \item{procure_loss}{Energy lost while procuring resource (kcal/min)}
+#'   \item{density}{Base density of resource on landscape (No. / km^2))}
+#'   \item{growth_rate}{Intrinsic rate of increase of resource (1/year)}
+#'   \item{search_radius}{Visibility of resource to forager (km)}
+#' }
+#' @source \url{https://www.sciencedirect.com/science/article/pii/0278416588900013}
+"wintPrey"
---
+++ R/diet_breadth.R
@@ -0,0 +1,91 @@
+#' @title Diet breadth model
+#'
+#' @description
+#' \itemize{
+#'   \item{dietBreadthOptimalSet determines the optimal set of resources to take on encounter}
+#'   \item{dietBreadthReturnRate calculates the return rate (kcal/hr) given a set of items in the diet.}
+#'   \item{dietBreadthOptimReturnRate calculates return rate for the optimal set.}
+#' }
+#'
+#' @details A forager searches the landscape for resources to procure. Each resource is encountered randomly as a Poisson process with encounter rate lambda_i. On encountering a resource, the forager chooses whether or not to procure it. If not, the forager continues searching. If so, the forager pursues and processes it with handling time h_i, receiving a value from the resource of v_i. Usually the value is the food energy provided by the item. Optionally, search is costly to the forager at the rate c_s_dot. That is, c_s_dot is the rate of loss of value (usually energy / time).
+#'
+#' The overall return rate of foraging is (using R syntax):
+#'
+#' returnRate = (-c_s_dot + sum(lambda * v)) / (1 + sum(lambda * h))
+#'
+#' The optimal diet is the set of items that maximizes this returnRate. Define
+#'
+#' rank_i = v_i / h_i
+#'
+#' as the rank (or on encounter return rate) of resource i. To determine the optimal set, order resources by rank from best to worst. Add resources to the diet in this rank order until the return rate no longer increases from adding additional items. This occurs when the on encounter return rate of a new item in the diet is lower than the overall return rate from procuring all the items that are more highly ranked.
+#'
+#' @references MacArthur and Pianka 1966 -- On Optimal Use of a Patchy Environment. American Naturalist. 100:603-609.
+
+#' @title Diet breadth optimal set
+#' @export
+#' @param v Vector of resource values
+#' @param h Vector of resource handling times
+#' @param lambda Vector of encounter rates
+#' @param c_s_dot Loss of value per unit time while searching (default 0)
+#' @return A boolean vector indicating whether each resource is in the optimal set
+dietBreadthOptimalSet <- function(v,h,lambda,c_s_dot=0) {
+  N <- length(v) # number of input resources potentially in optimal set
+
+  # Sort by rank (on encounter return rate)
+  rank_i <- v / h
+  ord <- order(rank_i,decreasing=T)
+  rank_i <- rank_i[ord]
+  v <- u_i[ord]
+  h <- t_i[ord]
+  lambda <- lambda[ord]
+
+  bestRate <- 0
+  bestSet <- rep(F,N)
+  testSet <- rep(F,N)
+  for(n in 1:N) {
+    testSet[n] <- T
+
+    testRate <- dietBreadthReturnRate(v,h,lambda,c_s_dot,testSet)
+    if(testRate >= bestRate) {
+        bestRate <- testRate
+        bestSet <- testSet
+    } else {
+        break
+    }
+  }
+  optimalSet <- bestSet
+  optimalSet[ord] <- optimalSet
+  return(optimalSet)
+}
+
+#' @title Diet breadth return rate
+#' @param v Vector of resource values
+#' @param h Vector of resource handling times
+#' @param lambda Vector of encounter rates
+#' @param c_s_dot Loss of value per unit time while searching (default 0)
+#' @return A boolean vector indicating whether each resource is in the optimal set
+#' @export
+dietBreadthReturnRate <- function(v,h,lambda,c_s_dot=0,include=NA) {
+  N <- length(v)
+  if(all(is.na(NA))) {
+    include <- rep(T,N)
+  }
+    
+  v <- u_i[include]
+  h <- t_i[include]
+  lambda <- lambda[include]
+  u_dot = (-c_s_dot + sum(lambda * v)) / (1 + sum(lambda * h))
+  return(u_dot)
+}
+
+#' @title Diet breadth optimal return rate
+#' @param v Vector of resource values
+#' @param h Vector of resource handling times
+#' @param lambda Vector of encounter rates
+#' @param c_s_dot Loss of value per unit time while searching (default 0)
+#' @return A boolean vector indicating whether each resource is in the optimal set
+#' @export
+dietBreadthOptimalReturnRate <- function(v,h,lambda,c_s_dot=0) {
+  include <- dietBreadthOptimalSet(v,h,lambda,c_s_dot)
+  return(dietBreadthReturnRate(v,h,lambda,c_s_dot,include))
+}
